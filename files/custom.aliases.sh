cf8a8c6f64f2c0302521db7e9e89899c6b7e7f41
Oliver Kelly <foolishhim@gmail.com>
2024-03-08 17:05:29 +0000

    updated aliases and gitconfig

--- files/custom.aliases.sh
+++ files/custom.aliases.sh
@@ -13,8 +13,17 @@ alias dtail='docker logs -tf --tail="50" "$@"'
 alias untar='tar -zxvf '
 alias newkey='ssh-keygen -t ed25519'
 alias konect="sshpass -p $MYPASS ssh -q"
+alias k="kubectl"
+alias wgup="sudo wg-quick up wg0"
+alias wgdown="sudo wg-quick down wg0"
+alias wgs="sudo wg"
+alias vi="nvim"
+alias vim="nvim"
 
-# Auto git pull when changing to folder
+# Alias to fix lsblk snap loop devices on Ubuntu
+alias lsblk="lsblk | grep -v loop*"# Auto git pull when changing to folder
+
+# Function to do a git pull when in a specified folder
 #function cd() {
 #    builtin cd "$@"
 #    if [ "$PWD" = "/home/USER/GIT_FOLDER" ]; then
