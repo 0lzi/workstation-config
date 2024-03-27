1b58c499ae1c052fe4a777415c02d6e0c9458e31
Oliver Kelly <foolishhim@gmail.com>
2024-03-27 15:10:08 +0000

    update nvim and tmux conf

--- files/.config/nvim/custom/init.lua
+++ files/.config/nvim/custom/init.lua
@@ -1,3 +1,13 @@
 vim.opt.colorcolumn="80"
 vim.wo.relativenumber = true
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = "→ ",
+  eol = "↲",
+  trail = "⋅",
+  extends = "❯",
+  precedes = "❮",
+  lead = "⋅",
+  nbsp = "%"
+}
 
--- files/.config/nvim/custom/mappings.lua
+++ files/.config/nvim/custom/mappings.lua
@@ -1,13 +1,16 @@
 local M = {}
 
 M.general = {
-  n = {
+  n = { -- Navigate nvim windows with vim hjkl
     ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
     ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
     ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
     ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+
+ -- Show whitespace
+    ["<leader>ll"] = { "<cmd> set list! <CR>", "Toggle whitespace"},
+
   }
 }
 
 return M
-
