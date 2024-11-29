af1758859654e7c845f3ec8ced4d5c1f445726fe
0lzi <foolishhim@gmail.com>
2024-11-29 21:11:53 +0000

    added lsps

--- files/.config/nvim/lua/configs/lspconfig.lua
+++ files/.config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,15 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html",
+  "cssls",
+  "bashls",
+  "dockerls",
+  "ansiblels",
+  "helm_ls",
+  "terraformls",
+  "yamlls"
+}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
