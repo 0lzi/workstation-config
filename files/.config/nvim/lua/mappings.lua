54b5b971342bea3162bb50a67d26f27f6155b1ee
Oliver Kelly <foolishhim@gmail.com>
2025-01-22 10:35:40 +0000

    update vimbinding

--- files/.config/nvim/lua/mappings.lua
+++ files/.config/nvim/lua/mappings.lua
@@ -2,6 +2,7 @@ require "nvchad.mappings"
 
 -- add yours here
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
@@ -28,3 +29,17 @@ map("n", "<leader>lg", "<cmd>LazyGit<CR>", { desc = "Open LazyGit"})
 -- Marp - Markdown presentations
 map("n", "<leader>mt", "<cmd>MarpToggle<CR>", { desc = "Toggle Marp Server"})
 map("n", "<leader>ms", "<cmd>MarpStatus<CR>", { desc = "Show Marp Server Status"})
+
+-- Move Code Up/Down
+--
+-- Normal mode
+map("n", "<A-j>", ":m .+1<CR>==", opts) -- Move current line/selection down
+map("n", "<A-k>", ":m .-2<CR>==", opts) -- Move current line/selection up
+
+-- Insert mode
+map("i", "<A-j>", "<Esc>:m .+1<CR>==gi", opts) -- Move current line/selection down and return to Insert mode
+map("i", "<A-k>", "<Esc>:m .-2<CR>==gi", opts) -- Move current line/selection up and return to Insert mode
+
+-- Visual mode
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", opts) -- Move selected block down
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", opts) -- Move selected block up
